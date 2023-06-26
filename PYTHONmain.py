77675e338346145767d5423bd6d7d2a585484afd
Ansh Lohani <lohaniansh23@gmail.com>
2023-06-27 01:04:33 +0530

    Update PYTHONmain.py
    added by ansh
    
    (not fully correct code)

--- PYTHONmain.py
+++ PYTHONmain.py
@@ -1,5 +1,40 @@
 import mysql.connector as sq
-con=sq.connect(host="localhost",user="root",passwd="dps@123",database="sehdev")
-if con.is_connected():
-	print("Conneted to SQL")
 
+#######################################################
+
+con=sq.connect(user="root",passwd="2006")
+if con.is_connected():
+    print("Connected to MYSQL")
+else:
+    print("Error!")
+cursor=con.cursor()
+con.commit()
+
+#created table in sql
+#cursor.execute("create table instagram(Name varchar(25),Followers int,Average_Likes int,Posts int);")
+
+########################################################
+
+
+#used the SocialMedia Database that we already created
+cursor.execute("use SocialMedia")
+
+
+#function to add values to the table: Currently not working
+def add_vals(name,followers,average_likes,posts):
+    cursor.execute("insert into instagram values('{}',{},{},{});".format(name,followers,average_likes,posts))
+    
+
+add_vals(name='Abhinav',followers=1,average_likes=1,posts=1)
+
+#########################################################
+
+#function to describe the table and print it into python: Currently not working
+def desc(base):
+    x= cursor.execute("desc {}".format(base))
+    return x
+
+print(desc(base='instagram'))
+
+
+########################################################
